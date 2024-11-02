fffb53eaf54674cea7d03dd4b67daa54682f0077
diegomadariaga <diegonicolasmadariaga@gmail.com>
2024-11-02 01:22:04 -0300

    feat: Implement findSafestPath function to calculate the safest path in a grid

--- halloween_03.ts
+++ halloween_03.ts
@@ -1,16 +1,34 @@
 function findSafestPath(dream: Array<number[]>) {
   const n = dream.length;
   const m = dream[0].length;
-  const deep = n + m - 1;
+  const paths: number[][] = [];
+  const traverse = (i: number, j: number, path: number[]) => {
+    if (i >= n || j >= m) return;
+    path.push(dream[i][j]);
 
-  return 0;
+    if (i === n - 1 && j === m - 1) {
+      paths.push([...path]);
+    } else {
+      traverse(i + 1, j, path); // Move down
+      traverse(i, j + 1, path); // Move right
+    }
+
+    path.pop();
+  };
+
+  traverse(0, 0, []);
+  return Math.min(
+    ...paths.map((path) => {
+      return path.reduce((acc, curr) => acc + curr, 0);
+    }),
+  );
 }
 
 const dream = [
-  [0, 1, 1, 1],
-  [0, 0, 0, 1],
-  [0, 0, 1, 3],
-  [0, 0, 0, 2],
+  [0, 1, 2, 3],
+  [4, 5, 6, 7],
+  [8, 9, 10, 11],
+  [12, 13, 14, 15],
 ];
 
 console.log(findSafestPath(dream));
