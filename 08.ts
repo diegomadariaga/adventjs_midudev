25a88e60f8d46ff8d850ffaff8131272ab472da5
diegomadariaga <diegonicolasmadariaga@gmail.com>
2024-12-21 23:50:57 -0300

    feat: Improve drawRace function by simplifying index handling and formatting output

--- 08.ts
+++ 08.ts
@@ -1,31 +1,25 @@
 function drawRace(indices: number[], length: number): string {
     const track = '~'.repeat(length);
     let result = '';
-    let spacesCounter = indices.length
+    let spacesCounter = indices.length;
+
     indices.forEach((index, i) => {
-        result += ' '.repeat(spacesCounter-1)
+        result += ' '.repeat(spacesCounter - 1);
         spacesCounter--;
+
         if (index === 0) {
             result += track;
-        }
-        // if index is negative or positive replace the character at that index with the "r"
-        else if (index < 0) {
-            result +=
-                track.slice(0, length + index) +
-                'r' +
-                track.slice(length + index + 1);
         } else {
-            result += track.slice(0, index) + 'r' + track.slice(index + 1);
+            const pos = index < 0 ? length + index : index;
+            result += track.slice(0, pos) + 'r' + track.slice(pos + 1);
         }
-        result += ' /' + (i + 1) + '\n';
-        if (i === 0) {
-            result = '' + result;
-        }
-        if (i === indices.length - 1) {
-            // elimina el ultimo salto de linea
-            result = result.slice(0, result.length - 1);
+
+        result += ' /' + (i + 1);
+        if (i < indices.length - 1) {
+            result += '\n';
         }
     });
+
     return result;
 }
 
