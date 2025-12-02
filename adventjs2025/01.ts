abcc3a41a507553a568c6b81d39fe0c7aef0c5fc
diegomadariaga <diegonicolasmadariaga@gmail.com>
2025-12-02 10:00:12 -0300

    feat: implement filterGifts function to return non-defective gifts

--- adventjs2025/01.ts
+++ adventjs2025/01.ts
@@ -23,5 +23,20 @@ console.log(good3)
 
 function filterGifts(gifts: string[]): string[] {
   // Code here
-  return []
+  return gifts.filter(gift => !gift.match(/[^a-zA-Z]/g));
 }
+
+const gifts1 = ['car', 'doll#arm', 'ball', '#train']
+const good1 = filterGifts(gifts1)
+console.log(good1)
+// ['car', 'ball']
+
+const gifts2 = ['#broken', '#rusty']
+const good2 = filterGifts(gifts2)
+console.log(good2)
+// []
+
+const gifts3 = []
+const good3 = filterGifts(gifts3)
+console.log(good3)
+// []
